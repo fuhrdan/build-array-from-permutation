3dafc7ef6470178be73cea12a45d4a41e80db476
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2025-05-06 06:35:25 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//*****************************************************************************
+//** 1920. Build Array from Permutation                             leetcode **
+//*****************************************************************************
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* buildArray(int* nums, int numsSize, int* returnSize) {
+    int* hashbrown = (int*)malloc(numsSize * sizeof(int));
+    if (hashbrown == NULL)
+    {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        hashbrown[i] = nums[nums[i]];
+    }
+
+    *returnSize = numsSize;
+    return hashbrown;
+}
